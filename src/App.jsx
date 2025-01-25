aa2f41c6623f2323c2894579c03de7f53fbc5acd
Rayuga Nub <75535740+RaYuGaa@users.noreply.github.com>
2025-01-25 17:27:36 +0545

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Toaster } from "sonner";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-import RegistrationForm from './Pages/Register';
-import Footer from './components/Footer';
 import DonationForm from "./components/DonationForm";
 import Contact from "./Pages/Contact";
 import Services from "./Pages/Services";
@@ -14,21 +12,20 @@ function App() {
   return (
     <Router>
       <ContactProvider>
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/register" element={<RegistrationForm />} />
-              <Route path="/donation" element={<DonationForm />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/services" element={<Services />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-        <Toaster richColors position="top-center" />
+        
+          <div>
+            <Navbar />
+            <main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/donation" element={<DonationForm />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/services" element={<Services />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
       </ContactProvider>
     </Router>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,20 @@
-import DisasterDashboard from "@/components/DisasterDashboard"
-import DonationForm from "@/components/DonationForm"
+import React from 'react';
+import DisasterDashboard from '../components/DisasterDashboard';
 import Scroll from '../components/Scroll';
 import Services from './Services';
 
-
 const Home = () => {
   return (
-    <div>
-      <DisasterDashboard/>
-      <Scroll/>
-      <Services/>
+    <div className="flex flex-col">
+      <div className="h-screen">
+        <DisasterDashboard />
+      </div>
+      <div className="relative z-10">
+        <Scroll />
+        <Services />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
